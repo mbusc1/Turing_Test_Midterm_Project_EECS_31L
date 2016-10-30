f7f20584ac10421be20fb9615eb216bdb2d25f4f
soberanc <soberanc@uci.edu>
2016-10-30 16:03:47 -0700

    Update flaggen.sv

--- project_1/project_1.srcs/sources_1/new/flaggen.sv
+++ project_1/project_1.srcs/sources_1/new/flaggen.sv
@@ -11,9 +11,10 @@ module flaggen(mode, op1, op2, carry, result, C_flag, Z_flag, S_flag, O_flag);
 
     //Code Starts Here
     assign C_flag = carry;
-    assign Z_flag = ((result[127:0] == 0)? Z_flag == 1 : Z_flag == 0); 
-    assign S_flag = ((result[127] == 0)? S_flag == 1 : S_flag == 0);
-    assign O_flag = ((mode == 0 && result[127] == 1 && op1[127] == 0 && op2[127]== 0)? O_flag == 1 : O_flag == 0);
-    assign O_flag = ((mode == 0 && result[127] == 0 && op1[127] == 1 && op2[127]== 1)? O_flag == 1 : O_flag == 0);
+    assign Z_flag = ((result[127:0] == 0)? 1 : 0); 
+    assign S_flag = ((result[127] == 0)? 1 : 0);
+    assign O_flag = ((mode == 0 && result[127] == 1 && op1[127] == 0 && op2[127]== 0)? 1 : 0);
+    assign O_flag = ((mode == 0 && result[127] == 0 && op1[127] == 1 && op2[127]== 1)? 1 : 0);
 
 endmodule
+
