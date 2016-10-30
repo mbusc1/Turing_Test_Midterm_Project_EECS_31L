61c9d81f6b287d3d3a0f65577fd6acd5e19985b3
mbusc1 <pianobuscemi@gmail.com>
2016-10-30 15:45:47 -0700

    working on 128 alu

--- project_1/project_1.srcs/sources_1/new/alu_128bit.sv
+++ project_1/project_1.srcs/sources_1/new/alu_128bit.sv
@@ -9,4 +9,16 @@ output logic c_flag ;
 output logic z_flag ;
 output logic o_flag ;
 output logic s_flag ;
+
+carrygen(opsel, mode, cin);
+
+genvar i;
+generate
+    for(i=0;i<(DWIDTH-1);i=i+1) begin: alu128
+       
+    end
+endgenerate
+
+flaggen(.mode(), .op1(), .op2(), .carry(), .result(), .O_flag(o_flag), .S_flag(s_flag), .C_flag(c_flag), .Z_flag(z_flag));
+
 endmodule
