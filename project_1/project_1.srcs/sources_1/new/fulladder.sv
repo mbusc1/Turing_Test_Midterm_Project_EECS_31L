0860eab54de59e2a7a4235502e77ac4d9024f436
soberanc <soberanc@uci.edu>
2016-10-30 10:38:47 -0700

    Update fulladder.sv

---
+++ project_1/project_1.srcs/sources_1/new/fulladder.sv
@@ -0,0 +1,10 @@
+module f_adder(
+    input logic a,
+    input logic b,
+    input logic c,
+    output logic S,
+    output logic C1
+    );
+    assign S = (c^(a^b));
+    assign C1 = (a&b)|((a^b)&c);
+endmodule
