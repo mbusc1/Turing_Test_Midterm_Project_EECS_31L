140ab95403895e3b65fef0fda0c2a189386ccaa1
mbusc1 <pianobuscemi@gmail.com>
2016-10-30 16:13:40 -0700

    more 128 changes

--- project_1/project_1.srcs/sources_1/new/alu_128bit.sv
+++ project_1/project_1.srcs/sources_1/new/alu_128bit.sv
@@ -10,15 +10,15 @@ output logic z_flag ;
 output logic o_flag ;
 output logic s_flag ;
 
-carrygen(opsel, mode, cin);
+carrygen(.opsel(opsel[2:0]), .mode(mode), .cin(cin));
 
 genvar i;
 generate
     for(i=0;i<(DWIDTH-1);i=i+1) begin: alu128
-       
+       alu_1bit(.op1(op1[i]), .op2(op2[i]), .opsel(opsel[2:0]), .cin(cin), .mode(mode), .result(result[i]), .cout(cout));
     end
 endgenerate
 
-flaggen(.mode(), .op1(), .op2(), .carry(), .result(), .O_flag(o_flag), .S_flag(s_flag), .C_flag(c_flag), .Z_flag(z_flag));
+flaggen(.mode(), .op1(op1[DWIDTH-1:0]), .op2(op2[DWIDTH-1:0]), .carry(cout), .result(result[DWIDTH-1:0]), .O_flag(o_flag), .S_flag(s_flag), .C_flag(c_flag), .Z_flag(z_flag));
 
 endmodule
