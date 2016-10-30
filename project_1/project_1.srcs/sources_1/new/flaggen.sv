6d292a4482cb4c3118318bd66bfb4c1c5c759dc4
soberanc <soberanc@uci.edu>
2016-10-30 16:40:51 -0700

    Update flaggen.sv

--- project_1/project_1.srcs/sources_1/new/flaggen.sv
+++ project_1/project_1.srcs/sources_1/new/flaggen.sv
@@ -7,11 +7,18 @@ module flaggen(mode, op1, op2, carry, result, C_flag, Z_flag, S_flag, O_flag);
     output logic C_flag;
     output logic Z_flag; 
     output logic S_flag;
-    output logic O_flag; 
-
+    output logic O_flag;
+    wire zero; 
+    parameter DWIDTH = 128;
     //Code Starts Here
     assign C_flag = carry;
-    assign Z_flag = ((result[127:0] == 0)? 1 : 0); 
+    genvar i; 
+    generate
+        for (i = 0; i < (DWIDTH-1); i=i+1) begin: g
+            assign zero = ((result[i] == 0 )? zero+0: 1);  
+        end 
+    endgenerate     
+    assign Z_flag = ((zero == 0)? 1 : 0); 
     assign S_flag = ((result[127] == 0)? 1 : 0);
     assign O_flag = ((mode == 0 && result[127] == 1 && op1[127] == 0 && op2[127]== 0)? 1 : 0);
     assign O_flag = ((mode == 0 && result[127] == 0 && op1[127] == 1 && op2[127]== 1)? 1 : 0);
