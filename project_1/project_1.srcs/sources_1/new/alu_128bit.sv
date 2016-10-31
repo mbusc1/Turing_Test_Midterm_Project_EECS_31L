7bf64db251adaaa48ab0624941ea6d279f6b0760
mhepler2019 <mhepler@uci.edu>
2016-10-30 17:18:21 -0700

    open elborated design
    made elborated design functional

--- project_1/project_1.srcs/sources_1/new/alu_128bit.sv
+++ project_1/project_1.srcs/sources_1/new/alu_128bit.sv
@@ -1,4 +1,4 @@
-module alu_128bit ( op1 , op2 , opsel , mode , result , c_flag , z_flag , o_flag , s_flag );
+module alu_128bit ( op1 , op2 , opsel , mode , result , c_flag , z_flag , o_flag , s_flag, carry );
 parameter DWIDTH = 128;
 input logic [ DWIDTH -1:0] op1 ;
 input logic [ DWIDTH -1:0] op2 ;
@@ -9,16 +9,17 @@ output logic c_flag ;
 output logic z_flag ;
 output logic o_flag ;
 output logic s_flag ;
+output logic carry[DWIDTH:0];
 
-carrygen(.opsel(opsel[2:0]), .mode(mode), .cin(cin));
+carrygen(.opsel(opsel[2:0]), .mode(mode), .cin(carry[0]));
 
 genvar i;
 generate
     for(i=0;i<(DWIDTH-1);i=i+1) begin: alu128
-       alu_1bit(.op1(op1[i]), .op2(op2[i]), .opsel(opsel[2:0]), .cin(cin), .mode(mode), .result(result[i]), .cout(cout));
+       alu_1bit(.op1(op1[i]), .op2(op2[i]), .opsel(opsel[2:0]), .cin(carry[i]), .mode(mode), .result(result[i]), .cout(carry[i+1]));
     end
 endgenerate
 
-flaggen(.mode(), .op1(op1[DWIDTH-1:0]), .op2(op2[DWIDTH-1:0]), .carry(cout), .result(result[DWIDTH-1:0]), .O_flag(o_flag), .S_flag(s_flag), .C_flag(c_flag), .Z_flag(z_flag));
+flaggen(.mode(mode), .op1(op1[DWIDTH-1:0]), .op2(op2[DWIDTH-1:0]), .carry(carry[DWIDTH]), .result(result[DWIDTH-1:0]), .O_flag(o_flag), .S_flag(s_flag), .C_flag(c_flag), .Z_flag(z_flag));
 
 endmodule
--- project_1/project_1.srcs/sources_1/new/flaggen.sv
+++ project_1/project_1.srcs/sources_1/new/flaggen.sv
@@ -1,17 +1,17 @@
 module flaggen(mode, op1, op2, carry, result, C_flag, Z_flag, S_flag, O_flag);
+    parameter DWIDTH = 128;
     input mode; 
-    input op1[127:0]; 
-    input op2[127:0];
-    input carry;
-    input result[127:0];
+    input [DWIDTH-1:0]op1; 
+    input [DWIDTH-1:0]op2;
+    input [DWIDTH-1:0]carry;
+    input [DWIDTH-1:0]result;
     output logic C_flag;
     output logic Z_flag; 
     output logic S_flag;
     output logic O_flag;
     wire zero; 
-    parameter DWIDTH = 128;
     //Code Starts Here
-    assign C_flag = carry;
+    assign C_flag = carry[DWIDTH];
     genvar i; 
     generate
         for (i = 0; i < (DWIDTH-1); i=i+1) begin: g
